79ef975f87c71e9698e2b4bc108f0af8879ed8fb
ahsan-dev <ahsanshamin6799@gmail.com>
2024-08-20 08:10:03 +0500

    Update MenuManagement.jsx

--- src/pages/dashboard/MenuManagement.jsx
+++ src/pages/dashboard/MenuManagement.jsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 import {
   Box,
   Flex,
@@ -21,9 +21,18 @@ import { useForm } from "@mantine/form";
 import { useNavigate } from "react-router-dom";
 import { colors } from "@/configs/theme.config";
 import Uploader from "@/assets/svg/uploader.svg";
+import GeneralModal from "@/components/modal/GeneralModal";
+import { useDisclosure, useMediaQuery } from "@mantine/hooks";
 
 const MenuManagement = () => {
   const navigate = useNavigate();
+  const [newCategory, setNewCategory] = useState("");
+  const [selectedCateg, setSelectedCateg] = useState("");
+  const lg = useMediaQuery("(max-width: 1024px)");
+  const md = useMediaQuery("(max-width: 768px)");
+
+  const [openedModal, { open: openModal, close: closeModal }] =
+    useDisclosure(false);
 
   const categoryList = [
     "Breakfast",
@@ -76,190 +85,250 @@ const MenuManagement = () => {
       form.values.images.filter((_, index) => index !== currentIndex)
     );
   };
+  const handleCategoryChange = (value, option) => {
+    if (value === "Add New Category") {
+      openModal();
+    } else {
+      setSelectedCateg(option.value);
+      form.setFieldValue("category", value);
+    }
+  };
+
+  const newCategoryHandler = () => {
+    // const data = {
+    //   newCategory: newCategory,
+    // };
+    // if (res?.success) {
+    //   setSelectedCateg(res?.data?.data?._id);
+    //   form.setFieldValue("catgeory", res?.data?.data?._id);
+    //   // await fetchCategories();
+    //   setNewCategory("");
+    //   closeModal();
+    // }
+  };
 
   return (
-    <Box className=" border-2 bg-[#fff] shadow-md rounded-md">
-      <IconArrowNarrowLeft
-        size={32}
-        className="mx-4 my-2"
-        stroke={2}
-        onClick={() => navigate(-1)}
-      />
-      <Box
-        style={{
-          height: "100%",
-        }}
-        className="px-16 py-2"
-      >
-        <Title order={3} py={3}>
-          Add Menu
-        </Title>
-
-        <TextInput
-          className="mb-2"
-          w={"70%"}
-          label="Title"
-          placeholder="Input placeholder"
+    <>
+      <Box className=" border-2 bg-[#fff] shadow-md rounded-md">
+        <IconArrowNarrowLeft
+          size={32}
+          className="mx-4 my-2"
+          stroke={2}
+          onClick={() => navigate(-1)}
         />
-        <Textarea
-          className="mb-2"
-          autosize
-          minRows={5}
-          maxRows={12}
-          w={"70%"}
-          label="Description"
-          placeholder="Input placeholder"
-        />
-        <Flex gap={"lg"}>
-          <div className="w-1/2">
-            <NumberInput
-              className="mb-2"
-              label="Servings"
-              placeholder="Enter Servings"
-              hideControls
-              allowNegative={false}
-            />
-            <NumberInput
-              className="mb-2"
-              label="Price"
-              placeholder="Enter Price"
-              hideControls
-              allowNegative={false}
-            />
-            <NumberInput
-              className="mb-2"
-              label="Stock"
-              placeholder="Enter Stock"
-              hideControls
-              allowNegative={false}
-            />
-            <NumberInput
-              className="mb-2"
-              label="Discount (%)"
-              placeholder="Enter discount percentage"
-              suffix="%"
-              hideControls
-              maxLength={3}
-              min={0}
-              allowNegative={false}
-            />
-            <Select
-              className="mb-2"
-              label="Product Category"
-              placeholder="Select category"
-              data={["Add New Category", ...categoryList]}
-              comboboxProps={{
-                transitionProps: { transition: "pop", duration: 200 },
-                shadow: "md",
-              }}
-              // {...form.getInputProps("category")}
-              // value={selectedCateg}
-              // onChange={(_value, options) => handleCategoryChange(_value, options)}
-            />
-            <TagsInput
-              className="mb-2"
-              label="Tags"
-              placeholder="Select Tags"
-              data={tagsList}
-              // {...form.getInputProps("brands")}
-            />
-          </div>
+        <Box
+          style={{
+            height: "100%",
+          }}
+          className="px-16 py-2"
+        >
+          <Title order={3} py={3}>
+            Add Menu
+          </Title>
 
-          <div className="flex-1 py-4 border-[1px] border-[#333] rounded-sm px-4 my-4">
-            <Dropzone
-              className="w-full"
-              bg={"#F8F9FA"}
-              onDrop={(files) => {
-                form.setFieldValue(`images`, [...form.values.images, ...files]);
-              }}
-              onReject={(files) => console.log("rejected files", files)}
-              maxSize={2 * 1024 ** 2}
-              accept={[
-                MIME_TYPES.png,
-                MIME_TYPES.jpeg,
-                MIME_TYPES.svg,
-                MIME_TYPES.gif,
-              ]}
-              radius={"md"}
-            >
-              <Group
-                justify="center"
-                gap="xl"
-                mih={180}
-                style={{ pointerEvents: "none" }}
+          <TextInput
+            className="mb-2"
+            w={md ? "100%" : "70%"}
+            label="Title"
+            placeholder="Input placeholder"
+          />
+          <Textarea
+            className="mb-2"
+            autosize
+            minRows={5}
+            maxRows={12}
+            w={md ? "100%" : "70%"}
+            label="Description"
+            placeholder="Input placeholder"
+          />
+          <Flex gap={"lg"} direction={lg ? "column" : "row"}>
+            <div className={`${md ? "w-full" : "w-1/2"} `}>
+              <NumberInput
+                className="mb-2"
+                label="Servings"
+                placeholder="Enter Servings"
+                hideControls
+                allowNegative={false}
+              />
+              <NumberInput
+                className="mb-2"
+                label="Price"
+                placeholder="Enter Price"
+                hideControls
+                allowNegative={false}
+              />
+              <NumberInput
+                className="mb-2"
+                label="Stock"
+                placeholder="Enter Stock"
+                hideControls
+                allowNegative={false}
+              />
+              <NumberInput
+                className="mb-2"
+                label="Discount (%)"
+                placeholder="Enter discount percentage"
+                suffix="%"
+                hideControls
+                maxLength={3}
+                min={0}
+                allowNegative={false}
+              />
+              <Select
+                className="mb-2"
+                label="Product Category"
+                placeholder="Select category"
+                data={["Add New Category", ...categoryList]}
+                comboboxProps={{
+                  transitionProps: { transition: "pop", duration: 200 },
+                  shadow: "md",
+                }}
+                {...form.getInputProps("category")}
+                value={selectedCateg}
+                onChange={(_value, options) =>
+                  handleCategoryChange(_value, options)
+                }
+              />
+              <TagsInput
+                className="mb-2"
+                label="Tags"
+                placeholder="Select Tags"
+                data={tagsList}
+                // {...form.getInputProps("brands")}
+              />
+            </div>
+
+            <div className="flex-1 py-4 border-[1px] border-[#333] rounded-sm px-4 my-4">
+              <Dropzone
+                className="w-full"
+                bg={"#F8F9FA"}
+                onDrop={(files) => {
+                  form.setFieldValue(`images`, [
+                    ...form.values.images,
+                    ...files,
+                  ]);
+                }}
+                onReject={(files) => console.log("rejected files", files)}
+                maxSize={2 * 1024 ** 2}
+                accept={[
+                  MIME_TYPES.png,
+                  MIME_TYPES.jpeg,
+                  MIME_TYPES.svg,
+                  MIME_TYPES.gif,
+                ]}
+                radius={"md"}
               >
-                <Flex
-                  direction="column"
+                <Group
                   justify="center"
-                  align="center"
-                  gap={10}
+                  gap="xl"
+                  mih={180}
+                  style={{ pointerEvents: "none" }}
                 >
-                  <Dropzone.Accept>
-                    <Image src={Uploader} />
-                  </Dropzone.Accept>
-                  <Dropzone.Reject>
-                    <IconX
-                      style={{
-                        width: rem(52),
-                        height: rem(52),
-                        color: "var(--mantine-color-red-6)",
-                      }}
-                      stroke={1.5}
-                    />
-                  </Dropzone.Reject>
-                  <Dropzone.Idle>
-                    <Image w={100} src={Uploader} />
-                  </Dropzone.Idle>
-
-                  <Text className="text-center" size="md" inline>
-                    Drag and Drop Files or Browse
-                  </Text>
-                </Flex>
-              </Group>
-            </Dropzone>
-
-            <div className="flex mt-4 flex-wrap gap-2">
-              {form?.values?.images?.length > 0 &&
-                form?.values?.images?.map((i, index) => (
-                  <div
-                    key={index}
-                    className="relative bg-[#F0F1F1] rounded-md px-4 py-8"
+                  <Flex
+                    direction="column"
+                    justify="center"
+                    align="center"
+                    gap={10}
                   >
-                    <div className="absolute top-2 right-1 text-white text-lg cursor-pointer">
-                      <IoIosCloseCircle
-                        onClick={() => removeImage(index)}
-                        fill="gray"
-                        size={22}
+                    <Dropzone.Accept>
+                      <Image src={Uploader} />
+                    </Dropzone.Accept>
+                    <Dropzone.Reject>
+                      <IconX
+                        style={{
+                          width: rem(52),
+                          height: rem(52),
+                          color: "var(--mantine-color-red-6)",
+                        }}
+                        stroke={1.5}
+                      />
+                    </Dropzone.Reject>
+                    <Dropzone.Idle>
+                      <Image w={100} src={Uploader} />
+                    </Dropzone.Idle>
+
+                    <Text className="text-center" size="md" inline>
+                      Drag and Drop Files or Browse
+                    </Text>
+                  </Flex>
+                </Group>
+              </Dropzone>
+
+              <div className="flex mt-4 flex-wrap gap-2">
+                {form?.values?.images?.length > 0 &&
+                  form?.values?.images?.map((i, index) => (
+                    <div
+                      key={index}
+                      className="relative bg-[#F0F1F1] rounded-md px-4 py-8"
+                    >
+                      <div className="absolute top-2 right-1 text-white text-lg cursor-pointer">
+                        <IoIosCloseCircle
+                          onClick={() => removeImage(index)}
+                          fill="gray"
+                          size={22}
+                        />
+                      </div>
+                      <Image
+                        h={80}
+                        w={100}
+                        fit="contain"
+                        src={
+                          // product?.product && isEdit ? i :
+                          URL?.createObjectURL(i)
+                        }
                       />
                     </div>
-                    <Image
-                      h={80}
-                      w={100}
-                      fit="contain"
-                      src={
-                        // product?.product && isEdit ? i :
-                        URL?.createObjectURL(i)
-                      }
-                    />
-                  </div>
-                ))}
+                  ))}
+              </div>
             </div>
-          </div>
-        </Flex>
+          </Flex>
 
-        <Flex justify={"end"} my={4}>
-          <Button
-            onClick={() => navigate("/dashboard/menu/list")}
-            color={colors.primary[100]}
-            radius="sm"
-            size="sm"
-            px={8}
-          >
-            Publish
-          </Button>
-        </Flex>
+          <Flex justify={"end"} my={4}>
+            <Button
+              onClick={() => navigate("/dashboard/menu/list")}
+              color={colors.primary[100]}
+              radius="sm"
+              size="sm"
+              px={8}
+            >
+              Publish
+            </Button>
+          </Flex>
+        </Box>
       </Box>
-    </Box>
+
+      <GeneralModal
+        size="sm"
+        opened={openedModal}
+        close={closeModal}
+        title={<p className="font-bold">Add New Category</p>}
+        component={
+          <Box className=" rounded-md">
+            <div>
+              <TextInput
+                placeholder="Enter new category"
+                value={newCategory}
+                onChange={(event) => setNewCategory(event.currentTarget.value)}
+                className="flex-1 w-full"
+              />
+
+              <Button
+                type="button"
+                loading={false}
+                disabled={!newCategory}
+                className="mt-2"
+                radius={"sm"}
+                variant="light"
+                color={colors.primary[100]}
+                onClick={newCategoryHandler}
+              >
+                Add
+              </Button>
+            </div>
+          </Box>
+        }
+      />
+    </>
   );
 };
 
