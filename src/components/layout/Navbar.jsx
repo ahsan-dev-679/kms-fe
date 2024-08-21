45cd48fe97e6a180f8042d53f2ecf07f6a56eb5b
ahsan-dev <ahsanshamin6799@gmail.com>
2024-08-21 13:52:47 +0500

    Update Navbar.jsx

--- src/components/layout/Navbar.jsx
+++ src/components/layout/Navbar.jsx
@@ -40,13 +40,13 @@ const Navbar = () => {
       <div className="top-0 sticky py-1 lg:py-2 w-full bg-[#fff] lg:relative z-50 border-b-2 mb-6">
         <nav className="z-10 sticky top-0 left-0 right-0 max-w-full px-5 py-2.5 lg:border-none lg:py-4   border-yellow-500">
           <div className="flex items-center justify-between   !w-full flex-1">
-            <button>
+            <Link to={"/"}>
               <div className="flex items-center space-x-2">
                 <h2 className="text-[#4FAE5A] font-bold text-2xl">
                   QuickBites
                 </h2>
               </div>
-            </button>
+            </Link>
             <div className="hidden lg:block">
               <ul className="flex space-x-10 text-base font-bold text-black/60">
                 <li className="hover:underline hover:underline-offset-4 hover:w-fit transition-all duration-100 ease-linear">
